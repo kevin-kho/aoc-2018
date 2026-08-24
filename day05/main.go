0518386ddf261e794ecb255d7319aac29e4ed9e5
Kevin Ho <kevinkh08@gmail.com>
2026-08-24 15:56:54 -0700

    day05 part 2 complete

--- day05/main.go
+++ day05/main.go
@@ -7,6 +7,11 @@ import (
 	"github.com/kevin-kho/aoc-utilities/common"
 )
 
+type Alpha struct {
+	Upper byte
+	Lower byte
+}
+
 func SolvePartOne(data []byte) int {
 	var stack []byte
 
@@ -25,6 +30,49 @@ func SolvePartOne(data []byte) int {
 
 }
 
+func SolvePartTwo(data []byte) int {
+
+	// Create alpha pairs to exclude
+	var alphas []Alpha
+	var i byte = 65
+	alphas = append(alphas, Alpha{
+		Upper: i,
+		Lower: i + 32,
+	})
+	for range 25 {
+		i++
+		alphas = append(alphas, Alpha{
+			Upper: i,
+			Lower: i + 32,
+		})
+	}
+
+	var filteredData [][]byte
+	for _, alpha := range alphas {
+		filteredData = append(filteredData, FilterData(data, alpha))
+	}
+
+	shortestPolymer := len(data)
+	for _, d := range filteredData {
+		shortestPolymer = min(shortestPolymer, SolvePartOne(d))
+	}
+
+	return shortestPolymer
+
+}
+
+func FilterData(data []byte, exclude Alpha) []byte {
+	var res []byte
+	for _, b := range data {
+		if b == exclude.Lower || b == exclude.Upper {
+			continue
+		}
+		res = append(res, b)
+	}
+
+	return res
+}
+
 func main() {
 
 	// data, err := common.ReadInput("inputExample.txt")
@@ -36,5 +84,8 @@ func main() {
 
 	res := SolvePartOne(data)
 	fmt.Println(res)
+
+	res2 := SolvePartTwo(data)
+	fmt.Println(res2)
 
 }
