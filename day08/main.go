1741502d32a8393725c05b7e70856a5aa132fa68
Kevin Ho <kevin.ho01@sap.com>
2026-08-31 15:17:16 -0700

    day08 part 2 complete

--- day08/main.go
+++ day08/main.go
@@ -12,6 +12,8 @@ import (
 type Node struct {
 	ChildCount    int
 	MetadataCount int
+	ChildrenIdx   []int
+	Children      []*Node
 }
 
 func CreateNode(childCount int, metadataCount int) Node {
@@ -86,6 +88,79 @@ func SolvePartOne(intArr []int) int {
 
 }
 
+func BuildTree(intArr []int) *Node {
+	var root *Node
+	var stack []*Node
+
+	for len(intArr) > 0 {
+		childCt := intArr[0]
+		metadataCt := intArr[1]
+		intArr = intArr[2:]
+
+		stack = append(stack, &Node{
+			ChildCount:    childCt,
+			MetadataCount: metadataCt,
+		})
+
+		for len(stack) > 0 && stack[len(stack)-1].ChildCount == 0 {
+
+			c := intArr[0:stack[len(stack)-1].MetadataCount]
+			intArr = intArr[stack[len(stack)-1].MetadataCount:]
+			stack[len(stack)-1].ChildrenIdx = c
+
+			// Pop the used up Node
+			if len(stack) == 1 {
+				root = stack[len(stack)-1]
+			}
+			node := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			// Decrement top of stack
+			if len(stack) > 0 {
+				stack[len(stack)-1].ChildCount -= 1
+				stack[len(stack)-1].Children = append(stack[len(stack)-1].Children, node)
+			}
+		}
+	}
+	return root
+
+}
+
+func SolvePartTwo(intArr []int) int {
+
+	root := BuildTree(intArr)
+
+	var dfs func(node *Node) int
+	dfs = func(node *Node) int {
+
+		// case: hit leaf node, sum the metadata
+		if len(node.Children) == 0 {
+			return SumIntArr(node.ChildrenIdx)
+		}
+
+		var childrenValues []int
+		for _, childNode := range node.Children {
+			childrenValues = append(childrenValues, dfs(childNode))
+		}
+
+		var total int
+		// Indexes are 1-based
+		// Add up the values only if index is within range
+		for _, i := range node.ChildrenIdx {
+			if i-1 < len(childrenValues) {
+				total += childrenValues[i-1]
+			}
+		}
+
+		return total
+
+	}
+
+	res := dfs(root)
+
+	return res
+}
+
 func main() {
 	// data, err := common.ReadInput("inputExample.txt")
 	data, err := common.ReadInput("input.txt")
@@ -101,5 +176,8 @@ func main() {
 
 	res := SolvePartOne(intArr)
 	fmt.Println(res)
+
+	res2 := SolvePartTwo(intArr)
+	fmt.Println(res2)
 
 }
