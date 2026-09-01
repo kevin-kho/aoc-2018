270940269bd7688944a66f2df146bdd166b2182c
Kevin Ho <kevin.ho01@sap.com>
2026-09-01 09:54:46 -0700

    day11 part 2 memo wip

--- day11/main.go
+++ day11/main.go
@@ -7,6 +7,11 @@ type Pos struct {
 	Y int
 }
 
+type PosMemo struct {
+	Pos
+	M int
+}
+
 func GetHundredDigit(num int) int {
 	if num < 100 {
 		return 0
@@ -61,7 +66,7 @@ func TotalizeThreeByThree(p Pos, grid map[Pos]int) int {
 func TotalizeMByM(p Pos, grid map[Pos]int, m int) int {
 	var total int
 	for x := p.X; x < p.X+m; x++ {
-		for y := p.Y; y < p.Y+3; y++ {
+		for y := p.Y; y < p.Y+m; y++ {
 			total += grid[Pos{
 				X: x,
 				Y: y,
@@ -97,6 +102,7 @@ func SolvePartOne(grid map[Pos]int) Pos {
 
 }
 
+// Too slow
 func SolvePartTwo(grid map[Pos]int) Pos {
 	var currMax int
 	var res Pos
@@ -119,8 +125,82 @@ func SolvePartTwo(grid map[Pos]int) Pos {
 	return res
 }
 
+// WIP; incorrect solution
+func SolvePartTwoMemo(grid map[Pos]int) PosMemo {
+	// Build memo base case M = 1
+	memo := make(map[PosMemo]int)
+	for p, v := range grid {
+		memo[PosMemo{
+			Pos: p,
+			M:   1,
+		}] = v
+	}
+
+	var recurse func(p PosMemo) int
+	recurse = func(p PosMemo) int {
+		// Exit condition: Out of bounds
+		if !(1 <= p.X && p.X < 301) || !(1 <= p.Y && p.Y < 301) {
+			return 0
+		}
+		// Exit condition: we already solved
+		if val, ok := memo[p]; ok {
+			return val
+		}
+
+		// Sum up current row
+		var val int
+		for x := p.X; x < p.X+p.M; x++ {
+			val += grid[Pos{X: x, Y: p.Y}]
+		}
+
+		// Sum up current column
+		for y := p.Y; y < p.Y+p.M; y++ {
+			val += grid[Pos{X: p.X, Y: y}]
+		}
+
+		// Subtract the doubly counted point
+		val -= grid[Pos{X: p.X, Y: p.Y}]
+
+		// Recurively call the diagonal
+		val += recurse(PosMemo{
+			Pos: Pos{
+				X: p.X + 1,
+				Y: p.Y + 1,
+			},
+			M: min(301-(p.X+1), 301-(p.Y+1)),
+		})
+
+		// Memoize it and return value
+		memo[p] = val
+		return memo[p]
+	}
+
+	// Loop through and solve
+	var currMax int
+	var res PosMemo
+	for x := 1; x < 301; x++ {
+		for y := 1; y < 301; y++ {
+			p := Pos{X: x, Y: y}
+			mMax := min(301-x, 301-y)
+			for m := 1; m < mMax+1; m++ {
+				pm := PosMemo{Pos: p, M: m}
+				val := recurse(pm)
+				if val > currMax {
+					res = pm
+					currMax = val
+				}
+			}
+		}
+	}
+	return res
+
+	// recurse(PosMemo{Pos: Pos{X: 1, Y: 1}, M: min(301-1, 301-1)})
+	// return PosMemo{}
+
+}
+
 func main() {
-	serialNumber := 18
+	serialNumber := 42
 
 	mp := CreateGrid(serialNumber)
 
@@ -130,5 +210,8 @@ func main() {
 	// // TLE
 	// res2 := SolvePartTwo(mp)
 	// fmt.Println(res2)
+
+	res2 := SolvePartTwoMemo(mp)
+	fmt.Println(res2)
 
 }
