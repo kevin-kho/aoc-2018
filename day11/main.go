410cce9bf6ea16a065e092a6f015b5743d91569f
Kevin Ho <kevin.ho01@sap.com>
2026-09-01 08:13:25 -0700

    day11 part 1 complete

--- day11/main.go
+++ day11/main.go
@@ -7,11 +7,34 @@ type Pos struct {
 	Y int
 }
 
+func GetHundredDigit(num int) int {
+	if num < 100 {
+		return 0
+	}
+
+	num = num / 100
+	digit := num % 10
+
+	return digit
+
+}
+
 func CreateGrid(serialNumber int) map[Pos]int {
 	mp := make(map[Pos]int)
 	for x := 1; x < 301; x++ {
 		for y := 1; y < 301; y++ {
-			fmt.Println(x, y)
+			rackId := x + 10
+			powerLevel := rackId * y
+			powerLevel += serialNumber
+			powerLevel *= rackId
+
+			digit := GetHundredDigit(powerLevel)
+			digit -= 5
+
+			mp[Pos{
+				X: x,
+				Y: y,
+			}] = digit
 		}
 	}
 
@@ -19,9 +42,51 @@ func CreateGrid(serialNumber int) map[Pos]int {
 
 }
 
-func main() {
-	serialNumber := 8
+func TotalizeThreeByThree(p Pos, grid map[Pos]int) int {
+	var total int
+	for x := p.X; x < p.X+3; x++ {
+		for y := p.Y; y < p.Y+3; y++ {
+			total += grid[Pos{
+				X: x,
+				Y: y,
+			}]
 
-	CreateGrid(serialNumber)
+		}
+	}
+
+	return total
+
+}
+
+func SolvePartOne(grid map[Pos]int) Pos {
+	var currMax int
+	var res Pos
+
+	// Top left value
+	for x := 1; x < 299; x++ {
+		for y := 1; y < 299; y++ {
+
+			p := Pos{X: x, Y: y}
+
+			val := TotalizeThreeByThree(p, grid)
+
+			if val > currMax {
+				res = p
+				currMax = val
+			}
+		}
+	}
+
+	return res
+
+}
+
+func main() {
+	serialNumber := 8868
+
+	mp := CreateGrid(serialNumber)
+
+	res := SolvePartOne(mp)
+	fmt.Println(res)
 
 }
